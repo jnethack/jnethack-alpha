2f13c2e3d1f7bac5b4b873249c17ce15bbd6f3c2
SHIRAKATA Kentaro <argrath@ub32.org>
2016-03-24 01:16:47 +0900

    version++

--- japanese/jpatchlevel.h
+++ japanese/jpatchlevel.h
@@ -8,6 +8,6 @@
 /* JNetHack may be freely redistributed.  See license for details. */
 
 #define JVERSION_MAJOR	0
-#define JVERSION_MINOR	3
+#define JVERSION_MINOR	4
 #define JPATCHLEVEL	0
 #define JEDITLEVEL	0
