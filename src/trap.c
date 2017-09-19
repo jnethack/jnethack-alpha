fe7c13868e0ac17509516be7f3c43f01877aeb1e
SHIRAKATA Kentaro <argrath@ub32.org>
2017-09-19 21:56:35 +0900

    fix #37411

--- src/trap.c
+++ src/trap.c
@@ -3837,7 +3837,10 @@ climb_pit()
         fill_pit(u.ux, u.uy);
         vision_full_recalc = 1; /* vision limits change */
     } else if (!rn2(2) && sobj_at(BOULDER, u.ux, u.uy)) {
+/*JP
         Your("%s gets stuck in a crevice.", body_part(LEG));
+*/
+        Your("%sÇÕäÑÇÍñ⁄Ç…ÇÕÇ‹Ç¡ÇΩÅD", body_part(LEG));
         display_nhwindow(WIN_MESSAGE, FALSE);
         clear_nhwindow(WIN_MESSAGE);
 /*JP
