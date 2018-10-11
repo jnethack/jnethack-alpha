d9e9ab53177b9980fec082e3981dd47b4703f20a
SHIRAKATA Kentaro <argrath@ub32.org>
2018-10-12 04:57:58 +0900

    add macosx

---
+++ japanese/set_lnx.sh
@@ -0,0 +1,2 @@
+find . -type f |xargs nkf -e --overwrite
+sh sys/unix/setup.sh sys/unix/hints/linux
---
+++ japanese/set_mac.sh
@@ -0,0 +1,3 @@
+brew link --overwrite gcc@5
+find . -type f -name '*.c' -or -name '*.h'|xargs -n1 nkf -Sw --cp932 --overwrite
+sh sys/unix/setup.sh sys/unix/hints/macosx10.10
