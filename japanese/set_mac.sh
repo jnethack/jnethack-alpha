f5749334efaa842d82aab70a112d7b49e6a9b6a7
SHIRAKATA Kentaro <argrath@ub32.org>
2019-06-14 16:34:36 +0900

    patch for building on mac with gcc-8

--- japanese/set_mac.sh
+++ japanese/set_mac.sh
@@ -1,3 +1,3 @@
-brew link --overwrite gcc@5
+brew link --overwrite gcc@8
 find . -type f -name '*.c' -or -name '*.h'|xargs -n1 nkf -Sw --cp932 --overwrite
 sh sys/unix/setup.sh sys/unix/hints/macosx10.10
